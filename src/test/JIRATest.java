5739b324629f4358152e7bfca2cbf67d3353f695
apiliuk <lohika.artur@98point6.com>
2019-10-07 16:50:22 +0300

    added WebDriver Manager

---
+++ src/test/JIRATest.java
@@ -0,0 +1,111 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class JIRATest {
+
+  WebDriver driver;
+
+  @BeforeTest
+  public void setUp() {
+    WebDriverManager.chromedriver().setup();
+    // Create a new instance of the Firefox driver
+    this.driver = new ChromeDriver();
+  }
+
+  // Login
+  private By userNameInput = By.xpath("//input[@name='os_username']");
+  private By passwordInput = By.xpath("//input[@name='os_password']");
+  private By loginButton = By.xpath("//input[@name='login']");
+  private By wrongPasswordLabel = By.xpath("//div[@class='aui-message aui-message-error']");
+
+  // Create Issue
+  private By createIssueButton = By.id("create_link");
+  private By projectInput = By.cssSelector("#project-field");
+  private By issueTypeInput = By.id("issuetype-field");
+  private By summaryInput = By.id("summary");
+  private By dataModeSource = By.xpath("//li[@data-mode='source']");
+  private By descriptionInput = By.xpath("//textarea[@name='description']");
+  private By createButton = By.cssSelector("#create-issue-submit");
+  private By issueSuccessfullyCreated = By.xpath("//div[@class='aui-message closeable aui-message-success aui-will-close']");
+
+
+  @Test
+  public void loginTest() {
+    this.driver.get("https://jira.hillel.it/login.jsp");
+    driver.findElement(userNameInput).sendKeys("webinar5");
+    driver.findElement(passwordInput).sendKeys("webinar5");
+    driver.findElement(loginButton).click();
+    Assert.assertEquals(driver.getCurrentUrl(), "https://jira.hillel.it/secure/Dashboard.jspa");
+  }
+
+  @Test
+  public void createIssue() throws InterruptedException {
+    this.driver.get("https://jira.hillel.it/login.jsp");
+    driver.findElement(userNameInput).sendKeys("webinar5");
+    driver.findElement(passwordInput).sendKeys("webinar5");
+    driver.findElement(loginButton).click();
+    Assert.assertEquals(driver.getCurrentUrl(), "https://jira.hillel.it/secure/Dashboard.jspa");
+
+    waitFor(createIssueButton, 3, 10).click();
+
+    enterText(projectInput, "QAAUTO-8", 3, 10);
+    enterText(issueTypeInput, "Test", 3, 10);
+    enterText(summaryInput, "This is an automatic test.", 3, 10);
+
+    driver.findElement(dataModeSource);
+    enterText(descriptionInput, "This is an automatic test.", 3, 10);
+    driver.findElement(createButton).click();
+
+    Assert.assertEquals(waitFor(issueSuccessfullyCreated, 3, 10).isDisplayed(), true);
+  }
+
+  @AfterTest
+  public void tearDown() {
+    // Close the driver
+    this.driver.quit();
+  }
+
+  private WebElement waitFor(By element, int retry, int timeoutSeconds) {
+    for (int i = retry; i > 0; i--) {
+      try {
+        driver.findElement(element);
+      } catch (Exception ex) {
+        try {
+          Thread.sleep(TimeUnit.SECONDS.toMillis(timeoutSeconds));
+          driver.findElement(element);
+        } catch (Exception ex2) {
+          System.out.println("Searching element" + element.toString() + ". Retry - " + (retry - i));
+          continue;
+        }
+      }
+      return driver.findElement(element);
+    }
+    return driver.findElement(element);
+  }
+
+  private void enterText(By element, String text, int retry, int timeoutSeconds) {
+    for (int i = retry; i > 0; i--) {
+      try {
+        driver.findElement(element).sendKeys(text);
+        System.out.println("Searching element" + element.toString() + ". Retry - " + (retry - i));
+      } catch (Exception ex) {
+        try {
+          System.out.println("Searching element" + element.toString() + ". Retry - " + (retry - i));
+          Thread.sleep(TimeUnit.SECONDS.toMillis(timeoutSeconds));
+          driver.findElement(element).sendKeys(text);
+        } catch (Exception ex2) {
+          System.out.println("Searching element" + element.toString() + ". Retry - " + (retry - i));
+          continue;
+        }
+      }
+    }
+  }
+
+}
