3b1c12c98f2401b94b1613aff5ec84e750b43ffb
archick12 <a.a.piluck@gmail.com>
2019-11-11 20:32:12 +0200

    added Data Provider

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -9,41 +9,46 @@ import static pages.PagesURLs.loginPage;
 
 public class LoginPage {
 
-  WebDriver driver = null;
+    WebDriver driver = null;
 
-  // Login
-  private By userNameInput = By.xpath("//input[@name='os_username']");
-  private By passwordInput = By.xpath("//input[@name='os_password']");
-  private By loginButton = By.xpath("//input[@name='login']");
-  private By wrongPasswordLabel = By.xpath("//div[@class='aui-message aui-message-error']");
+    // Login
+    private By userNameInput = By.xpath("//input[@name='os_username']");
+    private By passwordInput = By.xpath("//input[@name='os_password']");
+    private By loginButton = By.xpath("//input[@name='login']");
+    private By wrongPasswordLabel = By.xpath("//div[@class='aui-message aui-message-error']");
 
-  public LoginPage() {
-      // Выполняется самым первым
-    this.driver = WebDriverFactory.getDriver();
-  }
+    public LoginPage() {
+        // Выполняется самым первым
+        this.driver = WebDriverFactory.getDriver();
+    }
 
-  public void navigate() {
-    driver.get(loginPage);
-  }
+    public void navigate() {
+        driver.get(loginPage);
+    }
 
-  @Step
-  public void enterUserName(String name) {
-    driver.findElement(userNameInput).sendKeys(name);
-  }
+    @Step
+    public void enterUserName(String name) {
+        driver.findElement(userNameInput).sendKeys(name);
+    }
 
-  @Step
-  public void enterPassword(String password) {
-    driver.findElement(passwordInput).sendKeys(password);
-  }
+    @Step
+    public void enterPassword(String password) {
+        driver.findElement(passwordInput).sendKeys(password);
+    }
 
-  @Step
-  public void clickLogin() {
-    driver.findElement(loginButton).click();
-  }
+    @Step
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
 
-  public void loginToJira(String name, String password) {
-    enterUserName(name);
-    enterPassword(password);
-    clickLogin();
-  }
+    @Step
+    public boolean isErrorMessagePresent(String message) {
+        return message.contains(driver.findElement(wrongPasswordLabel).getText());
+    }
+
+    public void loginToJira(String name, String password) {
+        enterUserName(name);
+        enterPassword(password);
+        clickLogin();
+    }
 }
--- src/test/java/JIRATest.java
+++ src/test/java/JIRATest.java
@@ -8,52 +8,56 @@ import utils.WebDriverFactory;
 
 public class JIRATest extends BaseTest {
 
-  @Feature("Login")
-  @Test(groups = {"Regression"})
-  public void loginTest() {
-    LoginPage loginPage = new LoginPage();
-    loginPage.navigate();
-    loginPage.loginToJira("webinar5", "webinar5");
-    Assert.assertEquals(WebDriverFactory.getDriver().getCurrentUrl(), "https://jira.hillel.it/secure/Dashboard.jspa");
-    Assert.assertTrue(1 == 1);
-  }
+    @Feature("Login")
+    @Test(groups = {"Regression"})
+    public void loginTest() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.navigate();
+        loginPage.loginToJira("webinar5", "webinar5");
+        Assert.assertEquals(WebDriverFactory.getDriver().getCurrentUrl(), "https://jira.hillel.it/secure/Dashboard.jspa");
+        Assert.assertTrue(1 == 1);
+    }
 
-  @Feature("Issue")
-  @Test(groups = {"Regression", "SKIP"})
-  public void createIssue() throws InterruptedException {
-    LoginPage loginPage = new LoginPage();
-    loginPage.navigate();
-    loginPage.loginToJira("webinar5", "webinar5");
-    Assert.assertEquals(WebDriverFactory.getDriver().getCurrentUrl(), "https://jira.hillel.it/secure/Dashboard.jspa");
+    @Feature("Issue")
+    @Test(groups = {"Regression", "SKIP"})
+    public void createIssue() throws InterruptedException {
+        LoginPage loginPage = new LoginPage();
+        loginPage.navigate();
+        loginPage.loginToJira("webinar5", "webinar5");
+        Assert.assertEquals(WebDriverFactory.getDriver().getCurrentUrl(), "https://jira.hillel.it/secure/Dashboard.jspa");
 
-    NewIssuePage newIssuePage = new NewIssuePage();
-    newIssuePage.clickCreateNewIssueButton();
-    newIssuePage.enterProjectName("QAAUT-8");
-    newIssuePage.enterIssueType("Test");
-    newIssuePage.enterIssueSummary("Some Summary");
-    newIssuePage.enterIssueDescription("Some Desc");
-    newIssuePage.clickCreateIssue();
+        NewIssuePage newIssuePage = new NewIssuePage();
+        newIssuePage.clickCreateNewIssueButton();
+        newIssuePage.enterProjectName("QAAUT-8");
+        newIssuePage.enterIssueType("Test");
+        newIssuePage.enterIssueSummary("Some Summary");
+        newIssuePage.enterIssueDescription("Some Desc");
+        newIssuePage.clickCreateIssue();
 
-  }
+    }
 
-  @Feature("Issue")
-  @Test(groups = {"Regression", "SKIP"})
-  public void testToBeSkipped() throws InterruptedException {
+    @Feature("Issue")
+    @Test(groups = {"Regression", "SKIP"})
+    public void testToBeSkipped() throws InterruptedException {
 
-  }
+    }
 
-  @Feature("Login")
-  @Test(groups = {"Regression"}, dataProvider = "data-provider")
-  public void dataProviderTest(String parameter) {
-    System.out.println(parameter);
-  }
+    @Feature("Login")
+    @Test(groups = {"Regression"}, dataProvider = "data-provider")
+    public void unsuccessfulLogin(String userName, String password, String msg) {
+        System.out.println(msg);
+        LoginPage loginPage = new LoginPage();
+        loginPage.navigate();
+        loginPage.loginToJira(userName, password);
+        Assert.assertEquals(loginPage.isErrorMessagePresent(msg), true);
+    }
 
-  @DataProvider(name = "data-provider")
-  public Object[][] dataProviderData() {
-    return new Object[][]{
-        {"The first value"},
-        {"The second value"}
-    };
-  }
+    @DataProvider(name = "data-provider")
+    public Object[][] dataProviderData() {
+        return new Object[][]{
+                {"webinar5", "wrongPassword", "Sorry, your username and password are incorrect - please try again."},
+                {"wrongUserName", "webinar5", "Sorry, your username and password are incorrect - please try again."}
+        };
+    }
 }
 
